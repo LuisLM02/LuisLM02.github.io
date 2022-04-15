87e6d610380570887b50ac689e71c463e0a9de25
luislm <luis.lopez.martell.llm@outlook.com>
2022-04-15 17:53:24 -0500

    se agrego Bootstrap,Adobe XD y App peliculas

--- js/data/proyects.js
+++ js/data/proyects.js
@@ -1,6 +1,22 @@
 export const proyectos = [
+  {
+    id: 1,
+    img: "../img-slides/app_peliculas.png",
+    titulo: "Películas",
+    descripcion: "Una aplicación que muestra información de películas hecha con TypeScript, SASS, Bootstrap 5, HTML y Webpack para el empaquetado de archivos.",
+    link: "../../poyects/app_peliculas/index.html",
+    linkGit: "https://github.com/LuisLM02/App-Peliculas"
+  },
+  {
+    id: 2,
+    img: "../img-slides/app_pokedex.png",
+    titulo: "Pokedex",
+    descripcion: "Una aplicación Pokémon que trae información de una API hecha con HTML, TypeScript, Ajax, SASS y Webpack para el empaquetado de archivos.",
+    link: "../../poyects/app_pokedex/index.html",
+    linkGit: "https://github.com/LuisLM02/App-Podex"
+  },
     {
-      id: 1,
+      id: 3,
       img: "../img-slides/app_cronometro.png",
       titulo: "Cronómetro",
       descripcion: "Un cronómetro sencillo creado con HTML, CSS y JavaScript, el código en CSS trabaja en módulos para facilitar la comprensión del mismo.",
@@ -8,7 +24,7 @@ export const proyectos = [
       linkGit: "https://github.com/LuisLM02/App-Cronometro"
     },
     {
-      id: 2,
+      id: 4,
       img: "../img-slides/app_notas.png",
       titulo: "Notas",
       descripcion: "Una aplicación de notas que te permite crear, borrar y editar las notas guardadas, fue creada usando HTML, CSS, SASS y JavaScript con programación modular.",
@@ -16,19 +32,12 @@ export const proyectos = [
       linkGit: "https://github.com/LuisLM02/App-Notas"
     },
     {
-      id: 3,
+      id: 5,
       img: "../img-slides/app_calculadora.png",
       titulo: "Calculadora",
       descripcion: "Una calculadora con funciones básicas hecha con HTML, CSS, SASS y JavaScipt usando programación modular.",
       link: "../../poyects/app_calculadora/index.html",
       linkGit: "https://github.com/LuisLM02/App-Calculadora"
     },
-    {
-      id: 4,
-      img: "../img-slides/app_pokedex.png",
-      titulo: "Pokedex",
-      descripcion: "Una aplicación Pokémon que trae información de una API hecha con HTML, TypeScript, Ajax, SASS y Webpack para el empaquetado de archivos.",
-      link: "../../poyects/app_pokedex/index.html",
-      linkGit: "https://github.com/LuisLM02/App-Podex"
-    }
+    
 ]
--- js/data/skills.js
+++ js/data/skills.js
@@ -4,6 +4,8 @@ export const skills = [
   {"src":"../assets/javascript.svg","text":"JavaScript"},
   {"src":"../assets/sass.svg","text":"SASS"},
   {"src":"../assets/typescript.svg","text":"TypeScript"},
+  {"src":"../assets/bootstrap5.svg","text":"Bootstrap 5"},
+  {"src":"../assets/adobexd.svg","text":"Adobe XD"},
   {"src":"../assets/git.svg","text":"Git"},
   {"src":"../assets/github.svg","text":"GitHub"}
 ]
